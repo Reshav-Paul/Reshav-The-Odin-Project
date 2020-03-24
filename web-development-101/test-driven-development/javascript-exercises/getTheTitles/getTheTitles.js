9be5c7bf269ef748c3038e449eaf40b7715732cc
Reshav Paul <paulrishav65@gmail.com>
2020-03-24 04:19:52 -0700

    Changed folder name

--- web-development-101/test-driven-development/javascript-exercises/getTheTitles/getTheTitles.js
+++
@@ -1,6 +0,0 @@
-const getTheTitles = function(books) {
-    let titles = books.map(book => book.title);
-    return titles;
-}
-
-module.exports = getTheTitles;
--- web-development-101/test-driven-development/javascript-exercises/reverseString/reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(inputString) {
-    let outputString = '';
-    inputString.split('').forEach(element => {
-        outputString = element + outputString;
-    });
-    return outputString;
-}
-
-module.exports = reverseString
