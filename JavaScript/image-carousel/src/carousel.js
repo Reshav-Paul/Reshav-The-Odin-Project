ee8dc9b0ce086177efcc94b8c03da2cef8bc589a
Reshav Paul <paulrishav65@gmail.com>
2020-05-04 14:58:27 -0700

    Added nav dots

--- JavaScript/image-carousel/src/carousel.js
+++ JavaScript/image-carousel/src/carousel.js
@@ -2,8 +2,10 @@ function createCarousel(images) {
     const carousel = document.createElement('div');
     carousel.style.overflow = 'hidden';
     carousel.style.position = 'relative';
-    carousel.style.height = '500px';
+    carousel.style.height = '100%';
+    carousel.style.margin = '1rem';
     carousel.style.display = 'flex';
+    carousel.classList.add('carousel');
     let _currentIndex = 0;
 
     function _getImage(src) {
@@ -18,9 +20,9 @@ function createCarousel(images) {
         return imageDiv;
     }
     function _applyButtonStyles(button) {
+        button.style.position = 'absolute';
+        button.style.top = '50%';
         button.style.color = 'white';
-        button.style.backgroundColor = 'white';
-        button.style.color = 'black';
         button.style.height = '2rem';
         button.style.width = '2rem';
         button.style.borderRadius = '50%';
@@ -31,11 +33,26 @@ function createCarousel(images) {
         button.style.fontSize = '1.3rem';
         button.style.cursor = 'pointer';
     }
-    const nextButton = document.createElement('div');
-    nextButton.textContent = '>';
-    nextButton.style.position = 'absolute';
-    nextButton.style.top = '50%';
-    nextButton.style.right = '5%';
+    function _getNavDot(index) {
+        const navDot = document.createElement('div');
+        navDot.classList.add('carousel-nav-dot');
+        navDot.style.height = '1rem';
+        navDot.style.width = '1rem';
+        navDot.style.margin = '0 1rem';
+        navDot.style.borderRadius = '50%';
+        navDot.style.background = 'white';
+        navDot.addEventListener('mouseup', () => {
+            if (index === _currentIndex) return;
+            const firstImage = document.querySelector('.carousel-image:first-child');
+            _currentIndex = index;
+            firstImage.style.marginLeft = `calc(${-(index)} * 100%)`;
+        });
+        return navDot;
+    }
+
+    const nextButton = document.createElement('i');
+    nextButton.classList.add('fa', 'fa-chevron-right');
+    nextButton.style.right = '1rem';
     _applyButtonStyles(nextButton);
     nextButton.addEventListener('click', e => {
         _currentIndex = (_currentIndex + 1) % images.length;
@@ -43,11 +60,10 @@ function createCarousel(images) {
         firstImage.style.marginLeft = `calc(${-(_currentIndex)} * 100%)`;
     });
 
-    const previousButton = document.createElement('div');
-    previousButton.textContent = '<';
-    previousButton.style.position = 'absolute';
-    previousButton.style.top = '50%';
-    previousButton.style.left = '5%';
+    const previousButton = document.createElement('i');
+    previousButton.classList.add('fa', 'fa-chevron-left');
+
+    previousButton.style.left = '1rem';
     _applyButtonStyles(previousButton);
     previousButton.addEventListener('click', e => {
         _currentIndex = _currentIndex === 0? (images.length - 1) : (_currentIndex - 1);
@@ -55,11 +71,24 @@ function createCarousel(images) {
         firstImage.style.marginLeft = `calc(${-(_currentIndex)} * 100%)`;
     });
 
+    const navDotsDiv = document.createElement('div');
+    navDotsDiv.style.position = 'absolute';
+    navDotsDiv.style.bottom = '1rem';
+    navDotsDiv.style.left = '50%';
+    navDotsDiv.style.transform = 'translateX(-50%)';
+    navDotsDiv.style.display = 'flex';
+    navDotsDiv.style.justifyContent = 'center';
+    navDotsDiv.style.alignItems = 'center';
+
+    let _dotIndex = 0;
+
     images.forEach(image => {
         carousel.appendChild(_getImage(image));
+        navDotsDiv.appendChild(_getNavDot(_dotIndex++));
     });
     carousel.appendChild(previousButton);
     carousel.appendChild(nextButton);
+    carousel.appendChild(navDotsDiv);
     return carousel;
 }
 
--- JavaScript/image-carousel/src/index.js
+++ JavaScript/image-carousel/src/index.js
@@ -1,6 +1,7 @@
 import {createCarousel} from './carousel.js';
 
 const carouselContainer = document.getElementById('carousel-container');
+carouselContainer.style.height = '500px';
 const carousel = createCarousel([
     'https://cdn1-www.playstationlifestyle.net/assets/uploads/2020/05/ac-valhalla-god-of-war.jpg',
     'https://cdn3-www.playstationlifestyle.net/assets/uploads/2020/04/BossLogic-AC-Valhalla-setting-art.jpg',
