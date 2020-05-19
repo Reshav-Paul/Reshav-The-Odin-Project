391ef56b035b656cbdf995de4b439eea6ca0d87e
Reshav Paul <paulrishav65@gmail.com>
2020-05-19 01:46:02 -0700

    Removed some functions

--- JavaScript/battleship/src/factory-functions/player.js
+++ JavaScript/battleship/src/factory-functions/player.js
@@ -1,23 +1,11 @@
 const Player = function(_name) {
     const getName = () => _name;
-    const makeRandomMove = (gameboard) => {
-        for (let i = 0; i < 10; ++i) {
-            for (let j = 0; j < 10; ++j) {
-                const isHit = gameboard.recieveAttack(i, j);
-                if (isHit !== undefined) return isHit;
-            }
-        }
-    }
-
-    const makeMove = (gameboard, x, y) => {
-        return gameboard.recieveAttack(x, y);
-    }
 
     const getScore = (gameboard) => {
         return gameboard.nSunk() / gameboard.nShips();
     }
 
-    return {getName, makeRandomMove, makeMove, getScore};
+    return {getName, getScore};
 }
 
 export default Player;
--- JavaScript/battleship/src/factory-functions/player.test.js
+++ JavaScript/battleship/src/factory-functions/player.test.js
@@ -7,68 +7,26 @@ describe('player tests', () => {
         expect(player.getName()).toBe('foo')
     });
 
-    it('player makes random moves', () => {
-        const player = Player('foo');
-        const gameboard = Gameboard();
-        expect(player.makeRandomMove(gameboard)).toBe(0);
-    });
-
-    it('player makes moves', () => {
-        const player = Player('foo');
-        const gameboard = Gameboard();
-        gameboard.placeShip(0, 0, 4);
-
-        for (let i = 0; i < 4; i++) {
-            expect(player.makeMove(gameboard, 0, i)).toBe(cellTypes.ship);
-        }
-        for (let i = 0; i < 4; i++) {
-            expect(player.makeMove(gameboard, 0, i)).toBeUndefined();
-        }
-
-        expect(player.makeMove(gameboard, 0, 5)).toBe(cellTypes.water);
-        expect(player.makeMove(gameboard, 0, 5)).toBeUndefined();
-        expect(gameboard.allSunk()).toBe(true);
-    });
-
-    it('player gets undefined return for invalid moves', () => {
-        const player = Player('foo');
-        const gameboard = Gameboard();
-        gameboard.placeShip(0, 0, 4);
-
-        player.makeMove(gameboard, 0, 0);
-        player.makeMove(gameboard, 1, 0);
-
-        expect(player.makeMove(gameboard, 0, 0)).toBeUndefined();
-        expect(player.makeMove(gameboard, 1, 0)).toBeUndefined();
-
-        expect(player.makeMove(gameboard, 0, 10)).toBeUndefined();
-        expect(player.makeMove(gameboard, 10, 0)).toBeUndefined();
-        expect(player.makeMove(gameboard, 10, 10)).toBeUndefined();
-        expect(player.makeMove(gameboard, -1, 0)).toBeUndefined();
-        expect(player.makeMove(gameboard, 0, -1)).toBeUndefined();
-        expect(player.makeMove(gameboard, -1, -1)).toBeUndefined();
-    });
-
     it ('player has score', () => {
         const player = Player('foo');
         const gameboard = Gameboard();
         gameboard.placeShip(0, 0, 2);
         gameboard.placeShip(3, 4, 3);
 
-        player.makeMove(gameboard, 0, 0);
-        player.makeMove(gameboard, 1, 0);
+        gameboard.recieveAttack(0, 0);
+        gameboard.recieveAttack(1, 0);
         expect(player.getScore(gameboard)).toBe(0);
 
-        player.makeMove(gameboard, 1, 1);
-        player.makeMove(gameboard, 1, 2);
+        gameboard.recieveAttack(1, 1);
+        gameboard.recieveAttack(1, 2);
         expect(player.getScore(gameboard)).toBe(0);
 
-        player.makeMove(gameboard, 0, 1);
-        player.makeMove(gameboard, 3, 4);
+        gameboard.recieveAttack(0, 1);
+        gameboard.recieveAttack(3, 4);
         expect(player.getScore(gameboard)).toBeCloseTo(0.5);
 
-        player.makeMove(gameboard, 3, 5);
-        player.makeMove(gameboard, 3, 6);
+        gameboard.recieveAttack(3, 5);
+        gameboard.recieveAttack(3, 6);
         expect(player.getScore(gameboard)).toBeCloseTo(1);
     })
 })
