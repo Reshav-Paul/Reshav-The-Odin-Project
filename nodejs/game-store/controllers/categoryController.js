9c9f2726d5e6de9aa667a464012f8dc953abc17e
Reshav Paul <paulrishav65@gmail.com>
2020-09-25 02:02:44 -0700

    Added category creation handler

--- nodejs/game-store/controllers/categoryController.js
+++ nodejs/game-store/controllers/categoryController.js
@@ -1,9 +1,11 @@
 let async = require('async');
+let { body, validationResult } = require('express-validator');
+
 let Category = require('../models/Category');
 let Game = require('../models/Game');
 
 module.exports.index = function(req, res) {
-    res.redirect('/home/categories');
+    res.redirect('/categories');
 };
 
 module.exports.category_list = function (req, res, next) {
@@ -30,3 +32,28 @@ module.exports.category_detail = function (req, res, next) {
 module.exports.category_create_get = function(req, res, next) {
     res.render('category_create');
 };
+
+module.exports.category_create_post = [
+    body('name', 'Name must not be empty').trim().escape().isLength({min: 1}),
+    body('description', 'Description must not be empty').trim().escape().isLength({min: 1}),
+
+    function (req, res, next) {
+        const errors = validationResult(req);
+        const {name, abbreviation, description, imageUrl} = req.body;
+        const categoryData = {name, description};
+        if (imageUrl.length > 0) categoryData.imageUrl = imageUrl;
+        if (abbreviation.length > 0) categoryData.abbreviation = abbreviation;
+        
+        const category = new Category(categoryData);
+
+        if (!errors.isEmpty()) {
+            res.render('category_create', {category: category, errors: errors});
+            return;
+        } else {
+            category.save(function(err) {
+                if (err) return next(err);
+                res.redirect(category.url);
+            });
+        }
+    }
+];
--- nodejs/game-store/routes/inventory.js
+++ nodejs/game-store/routes/inventory.js
@@ -8,6 +8,7 @@ let game_controller = require('../controllers/gameController');
 router.get('/', category_controller.index);
 router.get('/categories', category_controller.category_list);
 router.get('/category/create', category_controller.category_create_get);
+router.post('/category/create', category_controller.category_create_post);
 router.get('/category/:id', category_controller.category_detail);
 
 // Game Routes
