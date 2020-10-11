400262338a9bbbd0dea4f7fb9bb197dad5949cf7
Reshav Paul <paulrishav65@gmail.com>
2020-10-10 23:48:17 -0700

    Added user and editor models

---
+++ nodejs/blog-api/models/editor.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+let EditorSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    username: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true}
+});
+
+EditorSchema.virtual('url').get(function() {
+    return '/editor/' + this._id;
+});
+
+EditorSchema.pre('save', function(next) {
+    bcrypt.genSalt(process.env.passwordHash, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, function(err, hashedPassword) {
+            if (err) return next(err);
+            this.password = hashedPassword;
+            next();
+        });
+    })    
+});
+
+module.exports = mongoose.model('Editor', EditorSchema);
---
+++ nodejs/blog-api/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: String
+});
+
+UserSchema.virtual('url').get(function() {
+    return '/user/' + this._id;
+});
+
+UserSchema.pre('save', function(next) {
+    bcrypt.genSalt(process.env.passwordHash, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, function(err, hashedPassword) {
+            if (err) return next(err);
+            this.password = hashedPassword;
+            next();
+        });
+    })    
+});
+
+module.exports = mongoose.model('User', UserSchema);
