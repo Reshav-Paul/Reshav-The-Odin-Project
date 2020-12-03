748b0c93c7ea819f4d5fa197a19bd2a33f9b319e
Reshav Paul <paulrishav65@gmail.com>
2020-12-02 22:20:38 -0800

    Fixed editor route auth in blog API

--- nodejs/blog-api/routes/editors.js
+++ nodejs/blog-api/routes/editors.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 const editorController = require('../controllers/editorController');
 const authController = require('../controllers/authController');
+const { validateUserIdInParam } = require('../helpers/middlewares');
 
 router.get('/', authController.jwt_auth, editorController.editor_list);
 
 router.post('/', editorController.editor_create);
 router.get('/:id', authController.jwt_auth, editorController.editor_details);
-router.put('/:id', authController.editor_jwt_auth, editorController.editor_update);
-router.delete('/:id', authController.editor_jwt_auth, editorController.editor_delete);
+router.put('/:id', authController.editor_jwt_auth, validateUserIdInParam, editorController.editor_update);
+router.delete('/:id', authController.editor_jwt_auth, validateUserIdInParam, editorController.editor_delete);
 
 router.get('/:id/posts', authController.jwt_auth, editorController.editor_posts);
 
